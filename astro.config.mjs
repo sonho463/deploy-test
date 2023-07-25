61bcaaed371fea4f61c1ba9c6fbf2d0d185c9359
sonho463 <son.ho463@gmail.com>
2023-07-25 09:16:55 +0900

    baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: 'webson.site/deploy/test'
+    base: 'webson.site/deploy-test'
 });
