c7aa1bc85c698ad385cf5b98785657be9418f341
sonho463 <son.ho463@gmail.com>
2023-07-25 10:12:57 +0900

    icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,25 +9,21 @@ import { defineConfig } from 'astro/config';
 
 // 共通設定
 const commonConfig = {
-    // 共通のビルドオプションなどを設定
-    // 例: buildOptions, plugins, components, etc.
-  };
-  
-  // 開発環境の設定
-  const devConfig = {
-    // 開発時の設定をここに記述
-    // 例: ソースマップの有効化、ホットリロードの設定など
-  };
-  
-  // 本番環境の設定
-  const prodConfig = {
-    // 本番デプロイ時の設定をここに記述
-    // 例: 最適化、パフォーマンス向上のための設定など
-    base: 'deploy-test'
-  };
-  
-  // 環境に応じて設定をマージ
-  export default process.env.NODE_ENV === "production"
+    
+};
+
+// 開発環境の設定
+const devConfig = {
+    
+};
+
+// 本番環境の設定
+const prodConfig = {
+    
+    base: 'deploy-test',
+};
+
+// 環境に応じて設定をマージ
+export default process.env.NODE_ENV === "production"
     ? { ...commonConfig, ...prodConfig }
     : { ...commonConfig, ...devConfig };
-  
